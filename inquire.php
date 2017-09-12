bfdeb41caa468cfd0e4e221c6dd1a2be612fa54b
shen.huang[黃鎮玄] <shen.huang@104.com.tw>
2017-09-12 18:14:14 +0800

    第一版已完成，已經過套版美化

--- inquire.php
+++ inquire.php
@@ -18,21 +18,27 @@
  $password = "";
  $dbname = "ncc";
 
- $num = "1002011009";
-
-try{
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
-    
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "SELECT ind_cat_no,cnt,max_month,min_month,avg_month,std_month
-            FROM expjob_indcat_month_group_view
-            WHERE ind_cat_no = :num
-            ";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(":num", $num);
-    $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_CLASS);
-}catch(PDOException $e) {
-    $result = $e . "QAQ";
+ $num = [$_POST['ind_cat_no']];
+ $regexp = "/^[\d]{10}$/";
+ $num = preg_grep($regexp, $num);  
+ 
+if ($num[0]) {
+    try{
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+        
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT ind_cat_no,cnt,max_month,min_month,avg_month,std_month
+                FROM expjob_indcat_month_group_view
+                WHERE ind_cat_no = :num
+                ";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(":num", $num[0]);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_CLASS);
+    }catch(Exception $e) {
+        $result = $e;
+    }
 }
+
+// $result = $result ;
 echo json_encode($result);
