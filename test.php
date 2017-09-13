5216e50c72f9ac5b964c203384669f05cfe3206c
shen.huang[黃鎮玄] <shen.huang@104.com.tw>
2017-09-13 19:30:12 +0800

    finish

---
+++ test.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Insert data  
+ * database : demo0906
+ * table : 104corp_engineer
+ * (date:0906)
+ *
+ * @category None
+ * @package  None
+ * @author   shen Huang <shen.huang@104.com.tw>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 5.6.31 
+ * @link     http://www.xxxx.com/
+ **/
+
+$a = 123;
+
+/**
+ * Photos
+ * 
+ * @category None
+ * @package  None
+ * @author   Shen Huang <shen.huang@104.com.tw>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 5.6.31 
+ * @link     http://www.xxxx.com/
+ */
+class Yo
+{
+
+}
